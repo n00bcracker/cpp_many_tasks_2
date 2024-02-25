6f1a8ebfbb6c07fa482950b2b7b07ae972c812c8
n00bcracker <zacpa@ya.ru>
2024-02-25 19:38:47 +0000

    completed range_sum

--- range-sum/range_sum.cpp
+++ range-sum/range_sum.cpp
@@ -1,10 +1,11 @@
 #include "range_sum.h"
+#include <cstdint>
 
 uint64_t RangeSum(uint64_t from, uint64_t to, uint64_t step) {
-    uint64_t result = 0;
-    while (from < to) {
-        result += from;
-        from += step;
+    if (to > from) {
+        uint64_t n = (to - 1 - from) / step + 1;
+        return (2 * from + (n - 1) * step) * n / 2 ;
+    } else {
+        return 0;
     }
-    return result;
 }
