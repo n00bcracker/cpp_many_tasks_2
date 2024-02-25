83d03b33769a9d2b98edb4928d7653410e53b576
n00bcracker <zacpa@ya.ru>
2024-02-25 19:44:13 +0000

    fix style

--- range-sum/range_sum.cpp
+++ range-sum/range_sum.cpp
@@ -4,7 +4,7 @@
 uint64_t RangeSum(uint64_t from, uint64_t to, uint64_t step) {
     if (to > from) {
         uint64_t n = (to - 1 - from) / step + 1;
-        return (2 * from + (n - 1) * step) * n / 2 ;
+        return (2 * from + (n - 1) * step) * n / 2;
     } else {
         return 0;
     }
