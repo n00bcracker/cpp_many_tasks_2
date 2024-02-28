d7bf6874c35021097240bd7ed61dc1946ab4463f
n00bcracker <zacpa@ya.ru>
2024-02-28 22:37:48 +0000

    fixed for

--- is-prime/is_prime.cpp
+++ is-prime/is_prime.cpp
@@ -11,7 +11,7 @@ constexpr size_t kThreadCnt = 16;
 std::atomic_flag is_not_prime;
 
 void ThreadFunc(uint64_t x, uint64_t l_bound, uint64_t r_bound) {
-    for (size_t i = l_bound; i < r_bound + 1; ++i) {
+    for (size_t i = l_bound; i < r_bound; ++i) {
         if (!is_not_prime.test()) {
             if (x % i == 0) {
                 is_not_prime.test_and_set();
@@ -31,16 +31,16 @@ bool IsPrime(uint64_t x) {
     auto bound = std::min(root + 6, x);
 
     is_not_prime.clear();
-    if ((bound - 1) / kThreadCnt < 10) {
+    if ((bound - 2) / kThreadCnt < 10) {
         ThreadFunc(x, 2, bound);
     } else {
         std::vector<std::thread> threads;
         uint64_t l_bound = 2;
-        uint64_t std_interval_len = (bound - 1) / kThreadCnt;
-        uint64_t full_threads_cnt = (bound - 1) % kThreadCnt;
+        uint64_t std_interval_len = (bound - 2) / kThreadCnt;
+        uint64_t full_threads_cnt = (bound - 2) % kThreadCnt;
         for (size_t i = 0; i < kThreadCnt; ++i) {
             auto interval_len = i < full_threads_cnt ? std_interval_len + 1 : std_interval_len;
-            threads.emplace_back(ThreadFunc, x, l_bound, l_bound + interval_len - 1);
+            threads.emplace_back(ThreadFunc, x, l_bound, l_bound + interval_len);
             l_bound += interval_len;
         }
 
