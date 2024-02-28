9348ae571d85605c3dc2e3d90a5002df1197a090
n00bcracker <zacpa@ya.ru>
2024-02-28 22:02:36 +0000

    completed is-prime

--- is-prime/is_prime.cpp
+++ is-prime/is_prime.cpp
@@ -2,7 +2,27 @@
 
 #include <cmath>
 #include <algorithm>
+#include <cstddef>
 #include <ranges>
+#include <vector>
+#include <atomic>
+#include <thread>
+
+constexpr size_t kThreadCnt = 16;
+std::atomic_flag is_not_prime;
+
+void ThreadFunc(uint64_t x, uint64_t l_bound, uint64_t r_bound) {
+    for (auto i : std::views::iota(l_bound, r_bound)) {
+        if (!is_not_prime.test()) {
+            if (x % i == 0) {
+                is_not_prime.test_and_set();
+                break;
+            }
+        } else {
+            break;
+        }
+    }
+}
 
 bool IsPrime(uint64_t x) {
     if (x < 2) {
@@ -10,10 +30,25 @@ bool IsPrime(uint64_t x) {
     }
     auto root = static_cast<uint64_t>(std::sqrt(x));
     auto bound = std::min(root + 6, x);
-    for (auto i : std::views::iota(uint64_t{2}, bound)) {
-        if (x % i == 0) {
-            return false;
+
+    is_not_prime.clear();
+    if ((bound - 1) / kThreadCnt < 10) {
+        ThreadFunc(x, 2, bound);
+    } else {
+        std::vector<std::thread> threads;
+        uint64_t l_bound = 2;
+        uint64_t std_interval_len = (bound - 1) / kThreadCnt;
+        uint64_t full_threads_cnt = (bound - 1) % kThreadCnt;
+        for (size_t i = 0; i < kThreadCnt; ++i) {
+            auto interval_len = i < full_threads_cnt ? std_interval_len + 1 : std_interval_len;
+            threads.emplace_back(ThreadFunc, x, l_bound, l_bound + interval_len - 1);
+            l_bound += interval_len;
+        }
+
+        for (auto& thread : threads) {
+            thread.join();
         }
     }
-    return true;
+
+    return !is_not_prime.test();
 }
