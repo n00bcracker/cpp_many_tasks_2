c40df7d9e20330182cb797c67d4ebc4b8fe8795d
n00bcracker <zacpa@ya.ru>
2024-03-02 19:44:14 +0000

    fix

--- hash-table/concurrent_hash_map.h
+++ hash-table/concurrent_hash_map.h
@@ -193,7 +193,7 @@ private:
     }
 
     static constexpr auto kDefaultConcurrencyLevel = 8;
-    static constexpr auto kLoadFactor = 1.5;
+    static constexpr auto kLoadFactor = 1.2;
 
     std::vector<std::list<std::pair<K, V>>> table_;
     Hash hasher_;
