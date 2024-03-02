2f7977473538d27c8ecb9f2ac2c2a5dcf63ce219
n00bcracker <zacpa@ya.ru>
2024-03-02 19:37:06 +0000

    completed hash-table

--- hash-table/concurrent_hash_map.h
+++ hash-table/concurrent_hash_map.h
@@ -1,7 +1,11 @@
 #pragma once
 
-#include <unordered_map>
+#include <cstddef>
 #include <mutex>
+#include <stdexcept>
+#include <vector>
+#include <list>
+#include <atomic>
 
 template <class K, class V, class Hash = std::hash<K>>
 class ConcurrentHashMap {
@@ -15,53 +19,184 @@ public:
     }
 
     ConcurrentHashMap(int expected_size, int expected_threads_count, const Hash& hasher = {})
-        :  // 0 because unordered_map doesn't contain constructor with hasher
-          table_{0, hasher} {
-        (void)expected_threads_count;
+        : table_(), hasher_(hasher), mutexes_(expected_threads_count) {
         if (expected_size != kUndefinedSize) {
-            table_.reserve(expected_size);
+            const size_t lists_cnt =
+                (((expected_size - 1) / kLoadFactor + 1) - 1 / expected_threads_count + 1) *
+                expected_threads_count;
+            table_ =
+                std::vector<std::list<std::pair<K, V>>>(lists_cnt, std::list<std::pair<K, V>>{});
+        } else {
+            table_ = std::vector<std::list<std::pair<K, V>>>(expected_threads_count,
+                                                             std::list<std::pair<K, V>>{});
         }
     }
 
     bool Insert(const K& key, const V& value) {
-        std::lock_guard lock{mutex_};
-        return table_.emplace(key, value).second;
+        const auto hash = hasher_(key);
+        const auto mutex_idx = hash % mutexes_.size();
+        bool res;
+        {
+            std::lock_guard lock(mutexes_[mutex_idx]);
+            const auto list_idx = hash % table_.size();
+
+            if ((res = InsertIntoList(list_idx, std::pair<K, V>(key, value)))) {
+                ++size_;
+            }
+        }
+
+        if (size_ / table_.size() > kLoadFactor) {
+            Rehash();
+        }
+
+        return res;
     }
 
     bool Erase(const K& key) {
-        std::lock_guard lock{mutex_};
-        return table_.erase(key);
+        const auto hash = hasher_(key);
+        const auto mutex_idx = hash % mutexes_.size();
+        std::lock_guard lock(mutexes_[mutex_idx]);
+        const auto list_idx = hash % table_.size();
+
+        if (EraseFromList(list_idx, key)) {
+            --size_;
+            return true;
+        } else {
+            return false;
+        }
     }
 
     void Clear() {
-        std::lock_guard lock{mutex_};
-        table_.clear();
+        for (int i = 0; i < std::ssize(mutexes_); ++i) {
+            mutexes_[i].lock();
+        }
+
+        for (size_t i = 0; i < table_.size(); ++i) {
+            table_[i].clear();
+        }
+
+        size_ = 0;
+
+        for (int i = std::ssize(mutexes_) - 1; i >= 0; --i) {
+            mutexes_[i].unlock();
+        }
     }
 
     std::pair<bool, V> Find(const K& key) const {
-        std::lock_guard lock{mutex_};
-        if (auto it = table_.find(key); it != table_.end()) {
-            return {true, it->second};
+        const auto hash = hasher_(key);
+        const auto mutex_idx = hash % mutexes_.size();
+        std::lock_guard lock(mutexes_[mutex_idx]);
+        const auto list_idx = hash % table_.size();
+
+        auto f_iter = FindPosInList(list_idx, key);
+        const auto& list = table_[list_idx];
+        if (f_iter != list.end() && f_iter->first == key) {
+            return {true, f_iter->second};
         } else {
             return {false, {}};
         }
     }
 
     V At(const K& key) const {
-        std::lock_guard lock{mutex_};
-        return table_.at(key);
+        auto elem = Find(key);
+        if (elem.first) {
+            return elem.second;
+        } else {
+            throw std::out_of_range("WTF");
+        }
     }
 
     size_t Size() const {
-        std::lock_guard lock{mutex_};
-        return table_.size();
+        return size_;
     }
 
     static constexpr auto kUndefinedSize = -1;
 
 private:
-    static constexpr auto kDefaultConcurrencyLevel = 8;
+    std::list<std::pair<K, V>>::const_iterator FindPosInList(size_t list_idx, const K& key) const {
+        const auto& list = table_[list_idx];
+        for (auto it = list.begin(); it != list.end(); ++it) {
+            if (it->first >= key) {
+                return it;
+            }
+        }
 
-    std::unordered_map<K, V, Hash> table_;
-    mutable std::mutex mutex_;
+        return list.end();
+    }
+
+    std::list<std::pair<K, V>>::iterator FindPosInList(size_t list_idx, const K& key) {
+        auto& list = table_[list_idx];
+        for (auto it = list.begin(); it != list.end(); ++it) {
+            if (it->first >= key) {
+                return it;
+            }
+        }
+
+        return list.end();
+    }
+
+    bool InsertIntoList(size_t list_idx, std::pair<K, V>&& elem) {
+        auto& list = table_[list_idx];
+        auto f_iter = FindPosInList(list_idx, elem.first);
+        if (f_iter == list.end() || f_iter->first != elem.first) {
+            list.emplace(f_iter, std::move(elem));
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    bool EraseFromList(size_t list_idx, const K& key) {
+        auto& list = table_[list_idx];
+        auto f_iter = FindPosInList(list_idx, key);
+        if (f_iter != list.end() && f_iter->first == key) {
+            list.erase(f_iter);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    void Rehash() {
+        for (int i = 0; i < std::ssize(mutexes_); ++i) {
+            mutexes_[i].lock();
+        }
+
+        table_.resize(table_.size() * 2);
+
+        for (size_t list_idx = 0; list_idx < table_.size(); ++list_idx) {
+            auto& list = table_[list_idx];
+            for (auto it = list.begin(); it != list.end();) {
+                const auto hash = hasher_(it->first);
+                const auto new_list_idx = hash % table_.size();
+                if (new_list_idx != list_idx) {
+                    auto elem = std::move(*it);
+                    InsertIntoList(new_list_idx, std::move(elem));
+                    if (it == list.begin()) {
+                        list.erase(it);
+                        it = list.begin();
+                    } else {
+                        auto remove_it = it;
+                        --it;
+                        list.erase(remove_it);
+                        ++it;
+                    }
+                } else {
+                    ++it;
+                }
+            }
+        }
+
+        for (int i = std::ssize(mutexes_) - 1; i >= 0; --i) {
+            mutexes_[i].unlock();
+        }
+    }
+
+    static constexpr auto kDefaultConcurrencyLevel = 8;
+    static constexpr auto kLoadFactor = 3;
+
+    std::vector<std::list<std::pair<K, V>>> table_;
+    Hash hasher_;
+    mutable std::vector<std::mutex> mutexes_;
+    std::atomic_size_t size_;
 };
