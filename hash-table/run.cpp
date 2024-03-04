b4c9db37869e2173983f047d7358560c7bf2ac79
Moskalenko Viktor <moskalenkoviktor@list.ru>
2024-03-04 19:11:01 +0300

    fix: add IgnoredRegexp to .clang-tidy

--- hash-table/run.cpp
+++ hash-table/run.cpp
@@ -12,43 +12,43 @@
 static constexpr auto kSeed = 82'944'584;
 
 TEST_CASE("Benchmark") {
-    const auto kNumThreads = GENERATE(2u, 4u, 8u);
+    const auto num_threads = GENERATE(2u, 4u, 8u);
     static constexpr auto kNumIterations = 1'000'000;
 
-    ConcurrentHashMap<int, int> map(kNumIterations, kNumThreads);
-    BENCHMARK("RandomInsertions:" + std::to_string(kNumThreads)) {
+    ConcurrentHashMap<int, int> map(kNumIterations, num_threads);
+    BENCHMARK("RandomInsertions:" + std::to_string(num_threads)) {
         Runner runner{kNumIterations};
-        for (auto i : std::views::iota(0u, kNumThreads)) {
+        for (auto i : std::views::iota(0u, num_threads)) {
             Random rand{kSeed + 10 * i};
             runner.Do([&map, rand]() mutable { map.Insert(rand(), 1); });
         }
     };
 
     map.Clear();
-    BENCHMARK("SpecialInsertions:" + std::to_string(kNumThreads)) {
+    BENCHMARK("SpecialInsertions:" + std::to_string(num_threads)) {
         Runner runner{kNumIterations};
         EqualLowBits elb{16};
         runner.Do([&map, elb]() mutable { map.Insert(elb(), 1); });
-        for (auto i : std::views::iota(1, static_cast<int>(kNumThreads))) {
+        for (auto i : std::views::iota(1, static_cast<int>(num_threads))) {
             Increment inc{100'000 * i};
             runner.Do([&map, inc]() mutable { map.Insert(inc(), 1); });
         }
     };
 
     map.Clear();
-    BENCHMARK("ManySearches:" + std::to_string(kNumThreads)) {
+    BENCHMARK("ManySearches:" + std::to_string(num_threads)) {
         Runner runner{kNumIterations};
         runner.Do([&map, rand = Random{kSeed - 1}]() mutable { map.Insert(rand(), 1); });
-        for (auto i : std::views::iota(1u, kNumThreads)) {
+        for (auto i : std::views::iota(1u, num_threads)) {
             Random rand{kSeed - i - 1, 0, 1'000'000};
             runner.Do([&map, rand]() mutable { map.Find(rand()); });
         }
     };
 
     map.Clear();
-    BENCHMARK("Deletions:" + std::to_string(kNumThreads)) {
+    BENCHMARK("Deletions:" + std::to_string(num_threads)) {
         Runner runner{kNumIterations};
-        for (auto i : std::views::iota(0u, kNumThreads)) {
+        for (auto i : std::views::iota(0u, num_threads)) {
             Random rand{kSeed + i / 2};
             if (i % 2) {
                 runner.Do([&map, rand]() mutable { map.Insert(rand(), 1); });
