5f75b986266f7c56792c82c00de8b1af91ba5513
n00bcracker <zacpa@ya.ru>
2024-04-20 23:57:52 +0000

    solved_portscan

--- portscan/main.cpp
+++ portscan/main.cpp
@@ -1,2 +1,21 @@
+#include <cactus/cactus.h>
+#include <chrono>
+#include <iostream>
+#include <string>
+#include <string_view>
+#include <vector>
+#include "cactus/io/view.h"
+#include "cactus/net/address.h"
+#include "portscan.h"
+
 int main() {
+    cactus::Scheduler scheduler;
+    scheduler.Run([&] {
+        cactus::SocketAddress addr("51.250.102.65", 11173);
+        auto conn = DialTCP(addr);
+        std::vector<char> msg(100);
+        conn->Read(cactus::View(msg));
+        std::cout << "Message: " << std::string_view(msg.data()) << std::endl;
+    });
+
 }
--- portscan/portscan.cpp
+++ portscan/portscan.cpp
@@ -1,6 +1,32 @@
 #include "portscan.h"
+#include <cstdint>
+
+#include "cactus/core/timer.h"
+#include "cactus/net/address.h"
+
+PortState CheckPort(const cactus::SocketAddress& addr, const cactus::Duration timeout) {
+    cactus::TimeoutGuard guard{timeout};
+    try {
+        auto conn = DialTCP(addr);
+        conn->Close();
+    } catch (const cactus::TimeoutException& ex) {
+        return PortState::TIMEOUT;
+    } catch (...) {
+        return PortState::CLOSED;
+    }
+
+    return PortState::OPEN;
+}
+
 
 Ports ScanPorts(const cactus::SocketAddress& remote, uint16_t start, uint16_t end,
                 cactus::Duration timeout) {
-    return {};
+    Ports res;
+    cactus::SocketAddress checking_address = remote;
+    for (uint16_t port = start; port < end; ++ port) {
+        checking_address.SetPort(port);
+        res.emplace_back(std::make_pair(port, CheckPort(checking_address, timeout)));
+    }
+
+    return res;
 }
