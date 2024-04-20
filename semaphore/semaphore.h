27ced2d91251640453f7048bcdf5c29505bd65d7
n00bcracker <zacpa@ya.ru>
2024-04-20 11:04:43 +0000

    completed semaphore

--- semaphore/semaphore.h
+++ semaphore/semaphore.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <cstddef>
 #include <mutex>
 #include <condition_variable>
 
@@ -10,8 +11,10 @@ public:
 
     void Acquire(auto callback) {
         std::unique_lock lock{mutex_};
-        cv_.wait(lock, [this] { return count_; });
+        size_t query_pos = jobs_cnt_++;
+        cv_.wait(lock, [&] { return count_ && query_pos == executing_pos_; });
         callback(count_);
+        ++executing_pos_;
     }
 
     void Acquire() {
@@ -21,11 +24,13 @@ public:
     void Release() {
         std::lock_guard lock{mutex_};
         ++count_;
-        cv_.notify_one();
+        cv_.notify_all();
     }
 
 private:
     int count_;
     std::mutex mutex_;
     std::condition_variable cv_;
+    size_t executing_pos_ = 0;
+    size_t jobs_cnt_ = 0;
 };
