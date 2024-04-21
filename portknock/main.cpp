a7bf3de7fe76c36a859c1d21f44bb8fc037eb5f4
n00bcracker <zacpa@ya.ru>
2024-04-21 14:06:54 +0000

    completed portknock

--- portknock/main.cpp
+++ portknock/main.cpp
@@ -1,2 +1,20 @@
+#include "portknock.h"
+#include <chrono>
+#include <iostream>
+
 int main() {
+    cactus::Scheduler scheduler;
+    scheduler.Run([&] {
+        cactus::SocketAddress addr("cpp-cactus-24.ru", 0, true);
+        Ports ports{
+            {10001, KnockProtocol::TCP}, {10022, KnockProtocol::TCP}, {10003, KnockProtocol::TCP}};
+        KnockPorts(addr, ports, std::chrono::seconds(3));
+
+        addr.SetPort(10080);
+        auto conn = DialTCP(addr);
+        std::vector<char> msg(100);
+        conn->Read(cactus::View(msg));
+        conn->Close();
+        std::cout << "Message: " << std::string_view(msg.data()) << std::endl;
+    });
 }
--- portknock/portknock.cpp
+++ portknock/portknock.cpp
@@ -1,4 +1,40 @@
 #include "portknock.h"
+#include <array>
+#include <exception>
+#include <iostream>
+#include "cactus/core/scheduler.h"
+#include "cactus/core/timer.h"
+#include "cactus/io/view.h"
+#include "cactus/net/net.h"
+
+namespace {
+
+void PortKnock(const cactus::SocketAddress& addr, const KnockProtocol protocol,
+               const cactus::Duration timeout) {
+    try {
+        std::array<char, 0> buf;
+        if (protocol == KnockProtocol::TCP) {
+            auto conn = DialTCP(addr);
+            conn->Write(cactus::View(buf));
+            conn->Close();
+        } else if (protocol == KnockProtocol::UDP) {
+            cactus::SocketAddress localhost("127.0.0.1", 7979, true);
+            auto conn = ListenUDP(localhost);
+            conn->SendTo(cactus::View(buf), addr);
+            conn->Close();
+        }
+    } catch (std::exception& ex) {
+        std::cout << ex.what() << std::endl;
+    }
+    cactus::SleepFor(timeout);
+}
+
+}  // namespace
 
 void KnockPorts(const cactus::SocketAddress& remote, const Ports& ports, cactus::Duration delay) {
+    cactus::SocketAddress cur_addr(remote);
+    for (const auto& [port, protocol] : ports) {
+        cur_addr.SetPort(port);
+        PortKnock(cur_addr, protocol, delay);
+    }
 }
