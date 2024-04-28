f4992501ba757b6c97542d06423409497edcb09a
n00bcracker <zacpa@ya.ru>
2024-04-28 16:19:11 +0000

    added flag

--- fanout/main.cpp
+++ fanout/main.cpp
@@ -1,2 +1,13 @@
+#include "fanout.h"
+#include <iostream>
+
 int main() {
+    cactus::Scheduler scheduler;
+    std::string msg;
+    scheduler.Run([&] {
+        cactus::SocketAddress server("cpp-cactus-24.ru", 13000, true);
+        msg = Fanout(server);
+    });
+
+    std::cout << "Message: " << msg << std::endl;
 }
