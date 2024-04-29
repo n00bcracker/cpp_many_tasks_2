34479f2df927b262668d2ee1ffc05eafc021955d
n00bcracker <zacpa@ya.ru>
2024-04-29 17:28:21 +0000

    rw-lock first try

--- rw-lock/rw_lock.h
+++ rw-lock/rw_lock.h
@@ -1,15 +1,20 @@
 #pragma once
 
+#include <condition_variable>
+#include <cstddef>
 #include <mutex>
 
 class RWLock {
 public:
     void Read(auto func) {
-        read_.lock();
-        if (++blocked_readers_ == 1) {
-            global_.lock();
-        }
-        read_.unlock();
+        std::unique_lock<std::mutex> check_lock{check_cnt_};
+        ++blocked_readers_cnt_;
+        can_read_.wait(check_lock, [this] { return !writers_cnt_; });
+
+        --blocked_readers_cnt_;
+        ++readers_cnt_;
+        check_lock.unlock();
+
         try {
             func();
         } catch (...) {
@@ -20,19 +25,46 @@ public:
     }
 
     void Write(auto func) {
-        std::lock_guard lock{global_};
-        func();
+        std::unique_lock<std::mutex> check_lock{check_cnt_};
+        ++blocked_writers_cnt_;
+        can_write_.wait(check_lock, [this] { return !readers_cnt_ && !writers_cnt_; });
+
+        --blocked_writers_cnt_;
+        ++writers_cnt_;
+        check_lock.unlock();
+
+        try {
+            func();
+        } catch (...) {
+            EndWrite();
+            throw;
+        }
+
+        EndWrite();
     }
 
 private:
     void EndRead() {
-        std::lock_guard lock{read_};
-        if (--blocked_readers_ == 0) {
-            global_.unlock();
+        std::lock_guard check_lock{check_cnt_};
+        if (--readers_cnt_ == 0 && blocked_writers_cnt_) {
+            can_write_.notify_one();
         }
     }
 
-    std::mutex read_;
-    std::mutex global_;
-    int blocked_readers_ = 0;
+    void EndWrite() {
+        std::lock_guard check_lock{check_cnt_};
+        --writers_cnt_;
+        if (blocked_readers_cnt_ > 4 * blocked_writers_cnt_) {
+            can_read_.notify_all();
+        } else if (blocked_writers_cnt_) {
+            can_write_.notify_one();
+        }
+    }
+
+    std::mutex check_cnt_;
+    std::condition_variable can_read_, can_write_;
+    size_t readers_cnt_ = 0;
+    size_t writers_cnt_ = 0;
+    size_t blocked_readers_cnt_ = 0;
+    size_t blocked_writers_cnt_ = 0;
 };
