e42fd73140145a3454422956bea2e97636c6cdd7
n00bcracker <zacpa@ya.ru>
2024-04-30 11:02:30 +0000

    changed implementation

--- timerqueue/timerqueue.h
+++ timerqueue/timerqueue.h
@@ -3,10 +3,10 @@
 #include <chrono>
 #include <condition_variable>
 #include <map>
-#include <memory>
 #include <mutex>
-#include <thread>
 #include <utility>
+
+using namespace std::chrono_literals;
 
 template <class T>
 class TimerQueue {
@@ -14,27 +14,20 @@ public:
     template <class... Args>
     void Add(std::chrono::system_clock::time_point at, Args&&... args) {
         std::unique_lock<std::mutex> add_elem_lock(edit_queue_);
-        if (!waiting_pop_) {
-            waiting_pop_ = std::shared_ptr<std::condition_variable>(new std::condition_variable());
-        }
 
         queue_.emplace(std::make_pair(at, T{std::forward<Args>(args)...}));
-
-        std::thread([waiting_pop = waiting_pop_, at] {
-            std::this_thread::sleep_until(at);
-            waiting_pop->notify_one();
-        }).detach();
+        waiting_pop_.notify_one();
     }
 
     T Pop() {
         std::unique_lock<std::mutex> pop_elem_lock(edit_queue_);
-        if (!waiting_pop_) {
-            waiting_pop_ = std::shared_ptr<std::condition_variable>(new std::condition_variable());
-        }
 
-        waiting_pop_->wait(pop_elem_lock, [this] {
-            return !queue_.empty() && std::chrono::system_clock::now() >= queue_.begin()->first;
-        });
+        waiting_pop_.wait_until(
+            pop_elem_lock,
+            queue_.empty() ? std::chrono::system_clock::now() + 10s : queue_.begin()->first,
+            [this] {
+                return !queue_.empty() && std::chrono::system_clock::now() >= queue_.begin()->first;
+            });
 
         T res(std::move(queue_.begin()->second));
         queue_.erase(queue_.begin());
@@ -43,6 +36,6 @@ public:
 
 private:
     std::mutex edit_queue_;
-    std::shared_ptr<std::condition_variable> waiting_pop_;
+    std::condition_variable waiting_pop_;
     mutable std::map<std::chrono::system_clock::time_point, T> queue_;
 };
