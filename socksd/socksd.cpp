a8036ba289c496085b80ec0e1dbd5848bcbf0f16
n00bcracker <zacpa@ya.ru>
2024-05-06 00:26:03 +0000

    complete socksd

--- socksd/socksd.cpp
+++ socksd/socksd.cpp
@@ -1,6 +1,5 @@
 #include "socksd.h"
 #include <cstddef>
-#include <exception>
 #include <memory>
 #include <stdexcept>
 #include <string>
@@ -10,36 +9,23 @@
 
 using namespace std::chrono_literals;
 
-size_t ReadWithTimeout(cactus::IConn* conn, cactus::MutableView buf) {
-    size_t read_byte_cnt = 0;
-    cactus::TimeoutGuard guard(300ms);
-    try {
-        while (!(read_byte_cnt = conn->Read(buf))) {
-            cactus::SleepFor(5ms);
-        }
+void ReadUserName(cactus::IConn* conn) {
+    std::string user_name(257, '\0');
+    size_t length = 0;
 
-        return read_byte_cnt;
-    } catch (const cactus::TimeoutException& ex) {
-        return 0;
-    } catch (...) {
-        throw;
-    }
-}
-
-size_t ReadWithBufLimit(cactus::IConn* conn, cactus::MutableView buf) {
-    size_t read_byte_cnt;
-    size_t all_read_cnt = 0;
-    while (buf.size()) {
-        read_byte_cnt = ReadWithTimeout(conn, buf);
-        if (read_byte_cnt) {
-            all_read_cnt += read_byte_cnt;
-            buf = buf.subspan(read_byte_cnt);
-        } else {
-            break;
+    do {
+        auto read_cnt = conn->Read(cactus::View(user_name.data() + length, 1));
+        if (!read_cnt) {
+            throw cactus::EOFException{};
         }
+        ++length;
+    } while (length < user_name.size() && user_name[length - 1] != 0);
+
+    if (user_name[length - 1] != 0) {
+        throw std::runtime_error("Wrong user name");
     }
 
-    return all_read_cnt;
+    user_name.resize(length - 1);
 }
 
 SocksServer::SocksServer(const cactus::SocketAddress& at) : lsn_(cactus::ListenTCP(at)) {
@@ -51,7 +37,7 @@ void TransferData(cactus::IConn* in_conn, cactus::IConn* out_conn) {
 
     try {
         while (true) {
-            read_byte_cnt = ReadWithTimeout(in_conn, cactus::View(buf));
+            read_byte_cnt = in_conn->Read(cactus::View(buf));
             if (!read_byte_cnt) {
                 break;
             }
@@ -85,16 +71,7 @@ void MakeConn(cactus::IConn* conn) {
         conn->ReadFull(cactus::View(port));
         conn->ReadFull(cactus::View(ip));
 
-        std::string user_name(257, '\0');
-        size_t user_length = ReadWithBufLimit(conn, cactus::View(user_name));
-        if (!user_length || user_name[user_length - 1] != 0) {
-            if (user_length == user_name.size()) {
-                throw std::runtime_error("Wrong user name");
-            } else {
-                throw cactus::EOFException{};
-            }
-        }
-        user_name.resize(user_length - 1);
+        ReadUserName(conn);
 
         cactus::SocketAddress endpoint(ip, htons(port));
         proxy_conn = cactus::DialTCP(endpoint);
@@ -124,13 +101,11 @@ void MakeConn(cactus::IConn* conn) {
     conn->Write(cactus::View(ip));
 
     cactus::WaitGroup proxy_group;
-    proxy_group.Spawn([in_conn = conn, out_conn = proxy_conn.get()] {
-        TransferData(in_conn, out_conn);
-    });
+    proxy_group.Spawn(
+        [in_conn = conn, out_conn = proxy_conn.get()] { TransferData(in_conn, out_conn); });
 
-    proxy_group.Spawn([in_conn = proxy_conn.get(), out_conn = conn] {
-        TransferData(in_conn, out_conn);
-    });
+    proxy_group.Spawn(
+        [in_conn = proxy_conn.get(), out_conn = conn] { TransferData(in_conn, out_conn); });
 
     proxy_group.Wait();
 }
@@ -138,9 +113,7 @@ void MakeConn(cactus::IConn* conn) {
 void SocksServer::Serve() {
     group_.Spawn([this] {
         while (std::shared_ptr conn = lsn_->Accept()) {
-            group_.Spawn([conn = std::move(conn)] {
-                MakeConn(conn.get());
-            });
+            group_.Spawn([conn = std::move(conn)] { MakeConn(conn.get()); });
         }
     });
 }
