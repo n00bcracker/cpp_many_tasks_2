cf9f749cfece1c68536930b41a2833740a4e1c5e
n00bcracker <zacpa@ya.ru>
2024-05-08 22:06:36 +0000

    final fix

--- compile-eval/sqrt.h
+++ compile-eval/sqrt.h
@@ -10,16 +10,21 @@ struct If<false, T, F> {
     static const int kValue = F::kValue;
 };
 
-template <unsigned long long L, unsigned long long R, int N>
+template <int L, int R, int N>
 struct BinSearchSqrt {
     static const int kValue =
-        If<((((L + R) / 2) * ((L + R) / 2)) < N), struct BinSearchSqrt<(L + R) / 2 + 1, R, N>,
+        If<((((L + R) / 2) < (N - 1) / ((L + R) / 2) + 1)), struct BinSearchSqrt<(L + R) / 2 + 1, R, N>,
            struct BinSearchSqrt<L, (L + R) / 2, N> >::kValue;
 };
 
-template <unsigned long long L, int N>
+template <int L, int N>
 struct BinSearchSqrt<L, L, N> {
     static const int kValue = L;
+};
+
+template <int L, int R>
+struct BinSearchSqrt<L, R, 1> {
+    static const int kValue = 1;
 };
 
 template <int N>
