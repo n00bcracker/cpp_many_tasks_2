c669a9b71df627925d7b8a92db4a10c91afdf979
n00bcracker <zacpa@ya.ru>
2024-05-09 22:00:58 +0000

    completed map

--- constexpr-map/constexpr_map.h
+++ constexpr-map/constexpr_map.h
@@ -1,26 +1,86 @@
 #pragma once
 
+#include <array>
+#include <cstddef>
+#include <stdexcept>
+#include <utility>
 template <class K, class V, int MaxSize = 8>
 class ConstexprMap {
 public:
     constexpr V& operator[](const K& key) {
+        for (size_t i = 0; i < size_; ++i) {
+            if (map_[i].first == key) {
+                return map_[i].second;
+            }
+        }
+
+        if (size_ < map_.size()) {
+            map_[size_].first = key;
+            return map_[size_++].second;
+        } else {
+            throw std::runtime_error("Map is full");
+        }
     }
 
     constexpr const V& operator[](const K& key) const {
+        for (size_t i = 0; i < size_; ++i) {
+            if (map_[i].first == key) {
+                return map_[i].second;
+            }
+        }
+
+        throw std::runtime_error("Key is missing");
     }
 
     constexpr bool Erase(const K& key) {
+        for (size_t i = 0; i < size_; ++i) {
+            if (map_[i].first == key) {
+                ShiftValues(i);
+                --size_;
+                return true;
+            }
+        }
+
+        return false;
     }
 
     constexpr bool Find(const K& key) const {
+        for (size_t i = 0; i < size_; ++i) {
+            if (map_[i].first == key) {
+                return true;
+            }
+        }
+
+        return false;
     }
 
     constexpr size_t Size() const {
+        return size_;
     }
 
     constexpr std::pair<K, V>& GetByIndex(size_t pos) {
+        if (pos < size_) {
+            return map_[pos];
+        } else {
+            throw std::runtime_error("Wrong index");
+        }
     }
 
     constexpr const std::pair<K, V>& GetByIndex(size_t pos) const {
+        if (pos < size_) {
+            return map_[pos];
+        } else {
+            throw std::runtime_error("Wrong index");
+        }
     }
+
+private:
+    constexpr void ShiftValues(size_t deleted_index) {
+        for (size_t i = deleted_index + 1; i < size_; ++i) {
+            map_[i - 1] = map_[i];
+        }
+    }
+
+    std::array<std::pair<K, V>, MaxSize> map_ = {};
+    size_t size_ = 0;
 };
--- constexpr-map/sort.h
+++ constexpr-map/sort.h
@@ -1,7 +1,42 @@
 #pragma once
 
+#include <cstddef>
+#include <utility>
 #include "constexpr_map.h"
 
 template <class K, class V, int S>
 constexpr auto Sort(ConstexprMap<K, V, S> map) {
+    auto sorted_map = map;
+    const auto sort_func = [&](bool ascending) {
+        if (sorted_map.Size() == 0) {
+            return;
+        }
+
+        for (size_t i = 0; i < sorted_map.Size() - 1; ++i) {
+            bool need_iter = false;
+            for (size_t j = 0; j < sorted_map.Size() - i - 1; ++j) {
+                if (ascending &&
+                    sorted_map.GetByIndex(j).first > sorted_map.GetByIndex(j + 1).first) {
+                    std::swap(sorted_map.GetByIndex(j), sorted_map.GetByIndex(j + 1));
+                    need_iter = true;
+                } else if (!ascending &&
+                           sorted_map.GetByIndex(j).first < sorted_map.GetByIndex(j + 1).first) {
+                    std::swap(sorted_map.GetByIndex(j), sorted_map.GetByIndex(j + 1));
+                    need_iter = true;
+                }
+            }
+
+            if (!need_iter) {
+                break;
+            }
+        }
+    };
+
+    if constexpr (std::is_integral_v<K>) {
+        sort_func(false);
+    } else {
+        sort_func(true);
+    }
+
+    return sorted_map;
 }
