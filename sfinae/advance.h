59cea6ec9ed71c23c91b9ec71639a7fdc460f222
n00bcracker <zacpa@ya.ru>
2024-05-11 18:23:35 +0000

    completed sfinae

--- sfinae/advance.h
+++ sfinae/advance.h
@@ -1,7 +1,51 @@
 #pragma once
 
 #include <cstddef>
+#include <iterator>
 
-template<class Iterator>
+template <typename InIterator>
+void AdvanceImpl(InIterator& iterator, ptrdiff_t n, std::input_iterator_tag) {
+    while (n > 0) {
+        ++iterator;
+        --n;
+    }
+}
+
+template <typename OutIterator>
+void AdvanceImpl(OutIterator& iterator, ptrdiff_t n, std::output_iterator_tag) {
+    while (n > 0) {
+        ++iterator;
+        --n;
+    }
+}
+
+template <typename FWDIterator>
+void AdvanceImpl(FWDIterator& iterator, ptrdiff_t n, std::forward_iterator_tag) {
+    while (n > 0) {
+        ++iterator;
+        --n;
+    }
+}
+
+template <typename BDIterator>
+void AdvanceImpl(BDIterator& iterator, ptrdiff_t n, std::bidirectional_iterator_tag) {
+    while (n > 0) {
+        ++iterator;
+        --n;
+    }
+
+    while (n < 0) {
+        --iterator;
+        ++n;
+    }
+}
+
+template <typename RAIterator>
+void AdvanceImpl(RAIterator& iterator, ptrdiff_t n, std::random_access_iterator_tag) {
+    iterator += n;
+}
+
+template <class Iterator>
 void Advance(Iterator& iterator, ptrdiff_t n) {
+    AdvanceImpl(iterator, n, typename std::iterator_traits<Iterator>::iterator_category());
 }
--- sfinae/clever_set.h
+++ sfinae/clever_set.h
@@ -1,20 +1,142 @@
 #pragma once
 
+#include <cstddef>
+#include <functional>
 #include <set>
+#include <type_traits>
+#include <unordered_set>
+#include <utility>
 
-template <class T>
+
+template <class T, typename Enable = void>
 class CleverSet {
 public:
     bool Insert(const T& value) {
-        return false;
+        return data_.insert(&value).second;
     }
 
     bool Erase(const T& value) {
-        return false;
+        if (data_.find(&value) != data_.end()) {
+            data_.erase(&value);
+            return true;
+        } else {
+            return false;
+        }
     }
 
     bool Find(const T& value) const {
-        return false;
+        if (data_.find(&value) != data_.end()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    size_t Size() const {
+        return data_.size();
+    }
+
+private:
+    std::unordered_set<const T*> data_;
+};
+
+template <typename T, typename Test = size_t>
+struct IsHashable {
+    static constexpr bool kValue = false;
+};
+
+template <typename T>
+struct IsHashable<T, decltype(std::hash<T>()(std::declval<T>()))> {
+    static constexpr bool kValue = true;
+};
+
+template <typename T>
+struct IsEqualable {
+    template <typename F, decltype(std::declval<F>() != std::declval<F>()) = true>
+    static bool Test1(const F&);
+
+    static void Test1(...);
+
+    template <typename F, decltype(std::declval<F>() == std::declval<F>()) = true>
+    static bool Test2(const F&);
+
+    static void Test2(...);
+
+    static constexpr bool kValue = std::is_same<bool, decltype(Test1(std::declval<T>()))>::value ||
+                                   std::is_same<bool, decltype(Test2(std::declval<T>()))>::value;
+};
+
+template <class T>
+class CleverSet<T, std::enable_if_t<IsHashable<T>::kValue && IsEqualable<T>::kValue>> {
+public:
+    bool Insert(const T& value) {
+        return data_.insert(value).second;
+    }
+
+    bool Erase(const T& value) {
+        if (data_.find(value) != data_.end()) {
+            data_.erase(value);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    bool Find(const T& value) const {
+        if (data_.find(value) != data_.end()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    size_t Size() const {
+        return data_.size();
+    }
+
+private:
+    std::unordered_set<T> data_;
+};
+
+template <typename T>
+struct IsComparable {
+    template <typename F, decltype(std::greater<>()(std::declval<F>(), std::declval<F>())) = true>
+    static bool Test1(const F&);
+
+    static void Test1(...);
+
+    template <typename F, decltype(std::less<>()(std::declval<F>(), std::declval<F>())) = true>
+    static bool Test2(const F&);
+
+    static void Test2(...);
+
+    static constexpr bool kValue = std::is_same<bool, decltype(Test1(std::declval<T>()))>::value ||
+                                   std::is_same<bool, decltype(Test2(std::declval<T>()))>::value;
+};
+
+template <class T>
+class CleverSet<T, std::enable_if_t<IsComparable<T>::kValue && !(IsHashable<T>::kValue && IsEqualable<T>::kValue)>> {
+public:
+    bool Insert(const T& value) {
+        return data_.insert(value).second;
+    }
+
+    bool Erase(const T& value) {
+        const auto it = data_.find(value);
+        if (it != data_.end()) {
+            data_.erase(value);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    bool Find(const T& value) const {
+        if (data_.find(value) != data_.end()) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
     size_t Size() const {
