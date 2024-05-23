69e3d7461344c3d1e8d6f77f5951cdf813f2362c
n00bcracker <zacpa@ya.ru>
2024-05-23 18:55:32 +0000

    completed ranges

--- ranges/get.h
+++ ranges/get.h
@@ -1,1 +1,222 @@
 #pragma once
+
+#include <concepts>
+#include <cstddef>
+#include <ranges>
+#include <iterator>
+#include <algorithm>
+#include <utility>
+
+namespace details {
+
+template <class T>
+struct Extractor {};
+
+template <class R, class T>
+struct Extractor<R T::*> {
+    using Result = R;
+    using Class = T;
+};
+
+
+template <std::ranges::forward_range R, typename FieldPtrType>
+    requires std::ranges::view<R>
+class GetFieldView : public std::ranges::view_interface<GetFieldView<R, FieldPtrType>> {
+    using FieldType = Extractor<FieldPtrType>::Result;
+    using BaseIteratorType = std::ranges::iterator_t<R>;
+
+    class Iterator {
+    public:
+        using iterator_category = std::iterator_traits<BaseIteratorType>::iterator_category;
+        using value_type = FieldType;
+        using difference_type = ptrdiff_t;
+        using pointer = value_type*;
+        using reference = value_type&;
+
+        Iterator() : it_(nullptr), field_ptr_(nullptr) {
+        }
+
+        Iterator(BaseIteratorType it, FieldPtrType field_ptr) : it_(it), field_ptr_(field_ptr) {
+        }
+
+        Iterator& operator++()
+            requires std::input_iterator<BaseIteratorType>
+        {
+            ++it_;
+            return *this;
+        }
+
+        void operator++(int)
+            requires std::input_iterator<BaseIteratorType>
+        {
+            ++*this;
+        }
+
+        value_type& operator*() const
+            requires std::input_iterator<BaseIteratorType>
+        {
+            return (*it_).*field_ptr_;
+        }
+
+        value_type* operator->() const
+            requires std::input_iterator<BaseIteratorType>
+        {
+            return &(it_->*field_ptr_);
+        }
+
+        Iterator operator++(int)
+            requires std::forward_iterator<BaseIteratorType>
+        {
+            Iterator it = *this;
+            ++(*this);
+            return it;
+        }
+
+        auto operator==(const Iterator& other) const
+            requires std::forward_iterator<BaseIteratorType>
+        {
+            return it_ == other.it_ && field_ptr_ == other.field_ptr_;
+        }
+
+        Iterator& operator--()
+            requires std::bidirectional_iterator<BaseIteratorType>
+        {
+            --it_;
+            return *this;
+        }
+
+        Iterator operator--(int)
+            requires std::bidirectional_iterator<BaseIteratorType>
+        {
+            Iterator it = *this;
+            --(*this);
+            return it;
+        }
+
+        Iterator& operator+=(int n)
+            requires std::random_access_iterator<BaseIteratorType>
+        {
+            it_ += n;
+            return *this;
+        }
+
+        Iterator operator+(int n) const
+            requires std::random_access_iterator<BaseIteratorType>
+        {
+            Iterator it = *this;
+            return it += n;
+        }
+
+        friend Iterator operator+(int n, const Iterator& it)
+            requires std::random_access_iterator<BaseIteratorType>
+        {
+            return it + n;
+        }
+
+        Iterator& operator-=(int n)
+            requires std::random_access_iterator<BaseIteratorType>
+        {
+            return *this += -n;
+        }
+
+        Iterator operator-(int n) const
+            requires std::random_access_iterator<BaseIteratorType>
+        {
+            Iterator it = *this;
+            return it -= n;
+        }
+
+        difference_type operator-(const Iterator& other) const
+            requires std::random_access_iterator<BaseIteratorType>
+        {
+            return it_ - other.it_;
+        }
+
+        value_type& operator[](size_t index) const
+            requires std::random_access_iterator<BaseIteratorType>
+        {
+            return *(it_ + index).*field_ptr_;
+        }
+
+        auto operator<(const Iterator& other) const
+            requires std::totally_ordered<BaseIteratorType>
+        {
+            return it_ < other.it_;
+        }
+
+        auto operator<=(const Iterator& other) const
+            requires std::totally_ordered<BaseIteratorType>
+        {
+            return it_ <= other.it_;
+        }
+
+        auto operator>(const Iterator& other) const
+            requires std::totally_ordered<BaseIteratorType>
+        {
+            return it_ > other.it_;
+        }
+
+        auto operator>=(const Iterator& other) const
+            requires std::totally_ordered<BaseIteratorType>
+        {
+            return it_ >= other.it_;
+        }
+
+    private:
+        BaseIteratorType it_;
+        FieldPtrType field_ptr_;
+    };
+
+public:
+    GetFieldView() = default;
+
+    GetFieldView(R base, FieldPtrType field_ptr) : base_{std::move(base)}, field_ptr_{field_ptr} {
+    }
+
+    auto begin() {
+        return Iterator(std::ranges::begin(base_), field_ptr_);
+    }
+
+    auto end() {
+        return Iterator(std::ranges::end(base_), field_ptr_);
+    }
+
+    auto size() const
+        requires std::ranges::sized_range<R>
+    {
+        return std::ranges::size(base_);
+    }
+
+private:
+    R base_;
+    FieldPtrType field_ptr_;
+};
+
+template <class R, typename FieldPtrType>
+GetFieldView(R&& base, FieldPtrType) -> GetFieldView<std::ranges::views::all_t<R>, FieldPtrType>;
+
+template <typename FieldPtrType>
+struct GetAdaptorClosure {
+    FieldPtrType field_ptr;
+
+    template <std::ranges::viewable_range R>
+    auto operator()(R&& r) {
+        return GetFieldView{std::forward<R>(r), field_ptr};
+    }
+
+    template <std::ranges::viewable_range R>
+    friend auto operator|(R&& r, GetAdaptorClosure closure) {
+        return GetFieldView{std::forward<R>(r), closure.field_ptr};
+    }
+};
+
+template <auto field>
+struct GetAdaptor : GetAdaptorClosure<decltype(field)> {
+
+    GetAdaptor() : GetAdaptorClosure<decltype(field)>{field} {};
+};
+
+}  // namespace details
+
+template <auto field>
+details::GetAdaptor<field> Get;
--- ranges/stride.h
+++ ranges/stride.h
@@ -1,1 +1,233 @@
 #pragma once
+
+#include <cstddef>
+#include <ranges>
+#include <concepts>
+
+namespace details {
+template <std::ranges::forward_range R>
+    requires std::ranges::view<R>
+class StrideView : public std::ranges::view_interface<StrideView<R>> {
+    using RangeValueT = std::ranges::range_value_t<R>;
+    using BaseIteratorType = std::ranges::iterator_t<R>;
+
+    class Iterator {
+    public:
+        using iterator_category = std::iterator_traits<BaseIteratorType>::iterator_category;
+        using value_type = RangeValueT;
+        using difference_type = ptrdiff_t;
+        using pointer = value_type*;
+        using reference = value_type&;
+
+        Iterator() = default;
+
+        Iterator(BaseIteratorType it, size_t pos, std::ranges::range_difference_t<R> step)
+            : it_(it), pos_(pos), k_(step) {
+        }
+
+        Iterator& operator++()
+            requires std::input_iterator<BaseIteratorType>
+        {
+            it_ = std::ranges::next(it_, k_);
+            ++pos_;
+            return *this;
+        }
+
+        void operator++(int)
+            requires std::input_iterator<BaseIteratorType>
+        {
+            ++*this;
+        }
+
+        value_type& operator*() const
+            requires std::input_iterator<BaseIteratorType>
+        {
+            return *it_;
+        }
+
+        value_type* operator->() const
+            requires std::input_iterator<BaseIteratorType>
+        {
+            return &(*it_);
+        }
+
+        Iterator operator++(int)
+            requires std::forward_iterator<BaseIteratorType>
+        {
+            Iterator it = *this;
+            ++(*this);
+            return it;
+        }
+
+        auto operator==(const Iterator& other) const
+            requires std::forward_iterator<BaseIteratorType>
+        {
+            return it_ == other.it_ && pos_ == other.pos_;
+        }
+
+        Iterator& operator--()
+            requires std::bidirectional_iterator<BaseIteratorType>
+        {
+            it_ = std::ranges::prev(it_, k_);
+            --pos_;
+            return *this;
+        }
+
+        Iterator operator--(int)
+            requires std::bidirectional_iterator<BaseIteratorType>
+        {
+            Iterator it = *this;
+            --(*this);
+            return it;
+        }
+
+        Iterator& operator+=(int n)
+            requires std::random_access_iterator<BaseIteratorType>
+        {
+            it_ = std::ranges::next(it_, k_ * n);
+            pos_ += n;
+            return *this;
+        }
+
+        Iterator operator+(int n) const
+            requires std::random_access_iterator<BaseIteratorType>
+        {
+            Iterator it = *this;
+            return it += n;
+        }
+
+        friend Iterator operator+(int n, const Iterator& it)
+            requires std::random_access_iterator<BaseIteratorType>
+        {
+            return it + n;
+        }
+
+        Iterator& operator-=(int n)
+            requires std::random_access_iterator<BaseIteratorType>
+        {
+            it_ = std::ranges::prev(it_, k_ * n);
+            pos_ -= n;
+            return *this;
+        }
+
+        Iterator operator-(int n) const
+            requires std::random_access_iterator<BaseIteratorType>
+        {
+            Iterator it = *this;
+            return it -= n;
+        }
+
+        difference_type operator-(const Iterator& other) const
+            requires std::random_access_iterator<BaseIteratorType>
+        {
+            return pos_ - other.pos_;
+        }
+
+        value_type& operator[](size_t index) const
+            requires std::random_access_iterator<BaseIteratorType>
+        {
+            Iterator it = (*this);
+            return *(it + index);
+        }
+
+        auto operator<(const Iterator& other) const
+            requires std::totally_ordered<BaseIteratorType>
+        {
+            return pos_ < other.pos_;
+        }
+
+        auto operator<=(const Iterator& other) const
+            requires std::totally_ordered<BaseIteratorType>
+        {
+            return pos_ <= other.pos_;
+        }
+
+        auto operator>(const Iterator& other) const
+            requires std::totally_ordered<BaseIteratorType>
+        {
+            return pos_ > other.pos_;
+        }
+
+        auto operator>=(const Iterator& other) const
+            requires std::totally_ordered<BaseIteratorType>
+        {
+            return pos_ >= other.pos_;
+        }
+
+    private:
+        BaseIteratorType it_;
+        size_t pos_;
+        std::ranges::range_difference_t<R> k_;
+    };
+
+public:
+    StrideView() = default;
+
+    StrideView(R base, std::ranges::range_difference_t<R> step)
+        : base_{std::move(base)}, step_{step} {
+    }
+
+    auto begin() {
+        return Iterator(std::ranges::begin(base_), 0, step_);
+    }
+
+    auto end() {
+        size_t i = 0;
+        auto it = std::ranges::begin(base_);
+        auto tmp = it;
+
+        while ((tmp = std::ranges::next(it, step_, std::ranges::end(base_))) !=
+               std::ranges::end(base_)) {
+            it = tmp;
+            ++i;
+        }
+
+        it = std::ranges::next(it, step_);
+        ++i;
+        return Iterator(it, i, step_);
+    }
+
+    auto size() const
+        requires std::ranges::sized_range<R>
+    {
+        auto n = std::ranges::size(base_);
+        return n > 0 ? (n - 1) / step_ + 1 : 1;
+    }
+
+private:
+    R base_;
+    std::ranges::range_difference_t<R> step_;
+};
+
+template <class R>
+StrideView(R&& base, std::ranges::range_difference_t<R>) -> StrideView<std::ranges::views::all_t<R>>;
+
+template <std::integral Difference>
+struct StrideAdaptorClosure {
+    Difference step;
+
+    template <std::ranges::viewable_range R>
+    auto operator()(R&& r) {
+        return StrideView{std::forward<R>(r), step};
+    }
+
+    template <std::ranges::viewable_range R>
+    friend auto operator|(R&& r, StrideAdaptorClosure closure) {
+        return StrideView{std::forward<R>(r), closure.step};
+    }
+};
+
+struct StrideAdaptor {
+    template <std::ranges::viewable_range R>
+    auto operator()(R&& r, std::ranges::range_difference_t<R> step) {
+        return StrideView{std::forward<R>(r), step};
+    }
+
+    template <std::integral T>
+    auto operator()(T step) {
+        return StrideAdaptorClosure<T>{step};
+    }
+};
+}  // namespace details
+
+details::StrideAdaptor Stride;
