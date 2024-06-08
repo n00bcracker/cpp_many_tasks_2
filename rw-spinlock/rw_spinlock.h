c0133a3ca339299dbaba132ec9f949ef3abeaef8
n00bcracker <zacpa@ya.ru>
2024-06-08 19:32:01 +0000

    completed rw_spinlock

--- rw-spinlock/rw_spinlock.h
+++ rw-spinlock/rw_spinlock.h
@@ -1,15 +1,32 @@
 #pragma once
 
+#include <atomic>
 class RWSpinLock {
+public:
     void LockRead() {
+        uint lock = rw_lock_.load();
+        do {
+            lock = (lock >> 1) << 1;
+        } while (!rw_lock_.compare_exchange_strong(lock, ((lock >> 1) + 1) << 1));
     }
 
     void UnlockRead() {
+        uint lock = rw_lock_.load();
+        do {
+        } while (!rw_lock_.compare_exchange_strong(lock, ((lock >> 1) - 1) << 1));
     }
 
     void LockWrite() {
+        uint lock = rw_lock_.load();
+        do {
+            lock = 0;
+        } while (!rw_lock_.compare_exchange_strong(lock, lock | 1));
     }
 
     void UnlockWrite() {
+        rw_lock_.store(0);
     }
+
+private:
+    std::atomic_uint rw_lock_;
 };
