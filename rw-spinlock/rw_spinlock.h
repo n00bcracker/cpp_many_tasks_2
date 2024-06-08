d6e52f1935e1ef41f3e4104d90ececf09188d3b7
n00bcracker <zacpa@ya.ru>
2024-06-08 20:17:08 +0000

    weak

--- rw-spinlock/rw_spinlock.h
+++ rw-spinlock/rw_spinlock.h
@@ -1,25 +1,26 @@
 #pragma once
 
 #include <atomic>
+#include <thread>
 class RWSpinLock {
 public:
     void LockRead() {
         uint lock = rw_lock_.load();
         do {
             lock = (lock >> 1) << 1;
-        } while (!rw_lock_.compare_exchange_strong(lock, ((lock >> 1) + 1) << 1));
+        } while (!rw_lock_.compare_exchange_weak(lock, ((lock >> 1) + 1) << 1));
     }
 
     void UnlockRead() {
         uint lock = rw_lock_.load();
-        do {
-        } while (!rw_lock_.compare_exchange_strong(lock, ((lock >> 1) - 1) << 1));
+        while (!rw_lock_.compare_exchange_weak(lock, ((lock >> 1) - 1) << 1)) {
+        }
     }
 
     void LockWrite() {
         uint lock = 0;
-        while (!rw_lock_.compare_exchange_strong(lock, 1)) {
-           lock = 0;
+        while (!rw_lock_.compare_exchange_weak(lock, 1)) {
+            lock = 0;
         }
     }
 
