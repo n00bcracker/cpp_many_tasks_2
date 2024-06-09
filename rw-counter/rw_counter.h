9e612d04aae464cb27135053c8e89f24d261c7b3
n00bcracker <zacpa@ya.ru>
2024-06-09 13:05:38 +0000

    completed rw_counter

--- rw-counter/rw_counter.h
+++ rw-counter/rw_counter.h
@@ -1,11 +1,15 @@
 #pragma once
 
 #include <atomic>
+#include <cstddef>
+#include <cstdint>
 #include <memory>
+#include <mutex>
+#include <vector>
 
 class Incrementer {
 public:
-    explicit Incrementer(std::atomic<int64_t>* counter) : counter_{counter} {
+    explicit Incrementer(std::shared_ptr<std::atomic<int64_t>> counter) : counter_{counter} {
     }
 
     void Increment() {
@@ -13,19 +17,31 @@ public:
     }
 
 private:
-    std::atomic<int64_t>* counter_;
+    std::shared_ptr<std::atomic<int64_t>> counter_;
 };
 
 class ReadWriteAtomicCounter {
 public:
     std::unique_ptr<Incrementer> GetIncrementer() {
-        return std::make_unique<Incrementer>(&counter_);
+        auto counter_ptr = std::make_shared<std::atomic<int64_t>>(0);
+        std::unique_lock lock(mutex_);
+        counters_.emplace_back(counter_ptr);
+        lock.unlock();
+        threads_cnt_ += 1;
+        return std::make_unique<Incrementer>(counter_ptr);
     }
 
     int64_t GetValue() const {
-        return counter_.load(std::memory_order_relaxed);
+        int64_t res = 0;
+        for (size_t i = 0; i < threads_cnt_; ++i) {
+            res += counters_[i]->load(std::memory_order_relaxed);
+        }
+
+        return res;
     }
 
 private:
-    std::atomic<int64_t> counter_ = 0;
+    std::vector<std::shared_ptr<std::atomic<int64_t>>> counters_;
+    std::mutex mutex_;
+    std::atomic_size_t threads_cnt_ = 0;
 };
