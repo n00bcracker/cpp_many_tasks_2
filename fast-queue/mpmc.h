2479f64a1f441dc77bffe696ff155ad8655217f9
n00bcracker <zacpa@ya.ru>
2024-06-12 16:57:02 +0000

    alignas

--- fast-queue/mpmc.h
+++ fast-queue/mpmc.h
@@ -9,16 +9,15 @@
 template <class T>
 class MPMCBoundedQueue {
 private:
-    struct Element {
+    struct alignas(16 * 1024) Element {
         T value;
         std::atomic_size_t generation;
     };
 
 public:
-    explicit MPMCBoundedQueue(size_t size) : max_size_(size), bit_mask_(max_size_ - 1) {
+    explicit MPMCBoundedQueue(size_t size) : max_size_(size), bit_mask_(max_size_ - 1), queue_(max_size_) {
         for (size_t i = 0; i < max_size_; ++i) {
-            queue_.emplace_back(std::make_unique<Element>());
-            queue_.back()->generation = i;
+            queue_[i].generation = i;
         }
     }
 
@@ -26,20 +25,20 @@ public:
         size_t tail = tail_;
         size_t index = tail & bit_mask_;
 
-        if (queue_[index]->generation.load(std::memory_order::acquire) + bit_mask_ <= tail) {
+        if (queue_[index].generation.load(std::memory_order::acquire) + bit_mask_ <= tail) {
             return false;
         }
 
         while (!tail_.compare_exchange_weak(tail, tail + 1, std::memory_order_acq_rel)) {
             std::this_thread::yield();
             index = tail & bit_mask_;
-            if (queue_[index]->generation.load(std::memory_order::acquire) + bit_mask_ <= tail) {
+            if (queue_[index].generation.load(std::memory_order::acquire) + bit_mask_ <= tail) {
                 return false;
             }
         }
 
-        queue_[index]->value = value;
-        queue_[index]->generation.fetch_add(1u, std::memory_order_release);
+        queue_[index].value = value;
+        queue_[index].generation.fetch_add(1u, std::memory_order_release);
         return true;
     }
 
@@ -47,20 +46,20 @@ public:
         size_t head = head_;
         size_t index = head & bit_mask_;
 
-        if (queue_[index]->generation.load(std::memory_order::acquire) < head + 1) {
+        if (queue_[index].generation.load(std::memory_order::acquire) < head + 1) {
             return false;
         }
 
         while (!head_.compare_exchange_weak(head, head + 1, std::memory_order_acq_rel)) {
             std::this_thread::yield();
             index = head & bit_mask_;
-            if (queue_[index]->generation.load(std::memory_order::acquire) < head + 1) {
+            if (queue_[index].generation.load(std::memory_order::acquire) < head + 1) {
                 return false;
             }
         }
 
-        data = queue_[index]->value;
-        queue_[index]->generation.fetch_add(bit_mask_, std::memory_order_release);
+        data = queue_[index].value;
+        queue_[index].generation.fetch_add(bit_mask_, std::memory_order_release);
         return true;
     }
 
@@ -69,5 +68,5 @@ private:
     const size_t bit_mask_;
     std::atomic_size_t head_ = 0;
     std::atomic_size_t tail_ = 0;
-    std::vector<std::unique_ptr<Element>> queue_;
+    std::vector<Element> queue_;
 };
