26b79bd2043263a125a7933e290f0d7505beb0c6
n00bcracker <zacpa@ya.ru>
2024-06-12 17:23:58 +0000

    128b

--- fast-queue/mpmc.h
+++ fast-queue/mpmc.h
@@ -8,7 +8,7 @@
 template <class T>
 class MPMCBoundedQueue {
 private:
-    struct alignas(1024) Element {
+    struct alignas(128) Element {
         std::atomic_size_t generation;
         T value;
     };
