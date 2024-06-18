514014fde4f216f4e5f66bd15701464b3e2c0ed9
n00bcracker <zacpa@ya.ru>
2024-06-18 22:04:44 +0000

    completed hazard_ptr

--- hazard-ptr/hazard_ptr.cpp
+++ hazard-ptr/hazard_ptr.cpp
@@ -1,9 +1,29 @@
 #include "hazard_ptr.h"
-
-std::shared_mutex lock;
+#include <mutex>
 
 void RegisterThread() {
+    ThreadState* state_ptr = new ThreadState{&hazard_ptr};
+    std::lock_guard lock(threads_lock);
+    threads.emplace(state_ptr);
 }
 
 void UnregisterThread() {
+    size_t threads_size;
+    {
+        std::lock_guard lock(threads_lock);
+        for (auto it = threads.begin(); it != threads.end(); ++it) {
+            auto* state_ptr = *it;
+            if (state_ptr->ptr == &hazard_ptr) {
+                it = threads.erase(it);
+                delete state_ptr;
+                break;
+            }
+        }
+
+        threads_size = threads.size();
+    }
+
+    if (threads_size == 0) {
+        ScanFreeList();
+    }
 }
--- hazard-ptr/hazard_ptr.h
+++ hazard-ptr/hazard_ptr.h
@@ -1,28 +1,103 @@
 #pragma once
 
+#include <algorithm>
 #include <atomic>
+#include <cstddef>
 #include <functional>
 #include <mutex>
-#include <shared_mutex>
 #include <memory>
+#include <unordered_set>
 
-extern std::shared_mutex lock;
+inline thread_local std::atomic<void*> hazard_ptr(nullptr);
+
+struct ThreadState {
+    std::atomic<void*>* ptr;
+};
+
+inline std::mutex threads_lock;
+inline std::unordered_set<ThreadState*> threads;
 
 void RegisterThread();
 void UnregisterThread();
 
 template <class T>
-T* Acquire(std::atomic<T*>* ptr) {
-    lock.lock_shared();
-    return ptr->load();
+inline T* Acquire(std::atomic<T*>* ptr) {
+    auto* value = ptr->load();
+
+    do {
+        hazard_ptr.store(value);
+
+        auto* new_value = ptr->load();
+        if (new_value == value) {
+            return value;
+        }
+
+        value = new_value;
+    } while (true);
 }
 
 inline void Release() {
-    lock.unlock_shared();
+    hazard_ptr.store(nullptr);
+}
+
+struct RetiredPtr {
+    void* value;
+    std::function<void()> deleter;
+    RetiredPtr* next;
+};
+
+inline std::atomic<RetiredPtr*> free_list = nullptr;
+inline std::atomic_size_t approximate_free_list_size = 0;
+constexpr size_t kFreeListMaxSize = 15;
+
+inline std::mutex scan_lock;
+
+inline void ScanFreeList() {
+    approximate_free_list_size.store(0);
+
+    if (!scan_lock.try_lock()) {
+        return;
+    }
+
+    std::lock_guard lock(scan_lock, std::adopt_lock);
+    auto* retired = free_list.exchange(nullptr);
+    std::vector<void*> hazard;
+    {
+        std::lock_guard guard{threads_lock};
+        for (auto* thread : threads) {
+            if (auto* ptr = thread->ptr->load()) {
+                hazard.push_back(ptr);
+            }
+        }
+    }
+
+    std::sort(hazard.begin(), hazard.end());
+
+    while (retired) {
+        auto* node = retired;
+        retired = retired->next;
+        if (!std::binary_search(hazard.begin(), hazard.end(), node->value)) {
+            node->deleter();
+            delete node;
+        } else {
+            node->next = free_list.load();
+            while (!free_list.compare_exchange_weak(node->next, node)) {
+            }
+        }
+    }
 }
 
 template <class T, class Deleter = std::default_delete<T>>
-void Retire(T* value, Deleter deleter = {}) {
-    std::lock_guard guard{lock};
-    deleter(value);
+inline void Retire(T* value, Deleter deleter = {}) {
+    RetiredPtr* retired_ptr =
+        new RetiredPtr{value, [deleter, value]() { deleter(value); }, free_list.load()};
+
+    while (!free_list.compare_exchange_weak(retired_ptr->next, retired_ptr)) {
+    }
+
+    approximate_free_list_size.fetch_add(1);
+
+    if (approximate_free_list_size > kFreeListMaxSize) {
+        ScanFreeList();
+    }
 }
