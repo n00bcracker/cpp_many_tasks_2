dc761c507ba0e27522b6ef56e788d6b38ab399c1
n00bcracker <zacpa@ya.ru>
2024-06-19 21:48:27 +0000

    completed lock-free-stack

--- lock-free-stack/stack.h
+++ lock-free-stack/stack.h
@@ -1,27 +1,61 @@
 #pragma once
 
-#include <stack>
+#include "../hazard-ptr/hazard_ptr.h"
+
 #include <mutex>
 
 template <class T>
 class Stack {
 public:
     void Push(const T& value) {
-        std::lock_guard guard{mutex_};
-        data_.push(value);
+        Node* old_head = head_;
+        Node* new_head = new Node{value, old_head};
+        while (!head_.compare_exchange_weak(old_head, new_head)) {
+            new_head->next = old_head;
+        }
     }
 
     bool Pop(T* value) {
-        std::lock_guard guard{mutex_};
-        if (data_.empty()) {
+        Node* old_head = Acquire(&head_);
+        if (!old_head) {
             return false;
         }
-        *value = std::move(data_.top());
-        data_.pop();
+
+        while (!head_.compare_exchange_weak(old_head, old_head->next)) {
+            old_head = Acquire(&head_);
+            if (!old_head) {
+                return false;
+            }
+        }
+
+        *value = std::move(old_head->value);
+        Release();
+        Retire(old_head);
         return true;
     }
 
+    void Clear() {
+        Node* old_head = head_;
+        while (!head_.compare_exchange_weak(old_head, nullptr)) {
+            old_head = head_;
+        }
+
+        while (old_head) {
+            Node* node = old_head;
+            old_head = old_head->next;
+            Retire(node);
+        }
+    }
+
+    ~Stack() {
+        Clear();
+    }
+
 private:
-    std::stack<T> data_;
-    mutable std::mutex mutex_;
+    struct Node {
+        T value;
+        Node* next;
+    };
+
+    std::atomic<Node*> head_ = nullptr;
 };
